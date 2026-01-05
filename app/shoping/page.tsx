a85b3e7dd4624f02c1fa943276c33c6a79e2173b
owaisqazi <waqqasqaziqazi@gmail.com>
2026-01-06 04:58:17 +0500

    ui and apis

--- app/shoping/page.tsx
+++ app/shoping/page.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import PageHeader from '@/components/sections/PageHeader';
 import Footer from '@/components/sections/Footer';
 import Navbar from '@/components/navbar/Navbar';
@@ -8,23 +5,9 @@ import { Toaster } from "react-hot-toast";
 import ShoppingCart from '@/components/sections/ShoppingCart';
 
 export default function page() {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const scrollToSection = (id: string) => {
-    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
     <main className="min-h-screen bg-white">
-     <Navbar scrolled={scrolled} scrollToSection={scrollToSection} /> 
+     <Navbar /> 
       <PageHeader title={"shoping Cart"} backgroundVideo = {"/about.mp4"}/>
       <ShoppingCart />
       <Footer />
