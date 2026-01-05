98ef94f747cba7fe3364f32f3ab67f8b5f692280
owaisqazi <waqqasqaziqazi@gmail.com>
2026-01-06 04:56:32 +0500

    ui and apis

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import PageHeader from '@/components/sections/PageHeader';
 import Contact from '@/components/sections/Contact';
 import Footer from '@/components/sections/Footer';
@@ -8,23 +5,9 @@ import Navbar from '@/components/navbar/Navbar';
 import { Toaster } from "react-hot-toast";
 
 export default function Home() {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  const scrollToSection = (id: string) => {
-    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
     <main className="min-h-screen bg-gradient-to-b from-amber-50 to-white">
-     <Navbar scrolled={scrolled} scrollToSection={scrollToSection} /> 
+     <Navbar /> 
       <PageHeader  title={"Contact Us"} backgroundImage = {"/gellery1.png"}/>
       <Contact />
       <Footer />
