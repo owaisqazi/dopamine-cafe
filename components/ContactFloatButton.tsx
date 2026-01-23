a2944e3070943680b15438edfd5115f3ddfeb083
owaisqazi <waqqasqaziqazi@gmail.com>
2026-01-23 08:01:06 +0500

    all done

--- components/ContactFloatButton.tsx
+++ components/ContactFloatButton.tsx
@@ -7,7 +7,7 @@ const ContactFloatButton = () => {
 
   return (
     <div
-      className="fixed bottom-5 right-5 z-50 flex flex-col items-end gap-3"
+      className="fixed bottom-3 right-5 z-50 flex flex-col items-end gap-3"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
