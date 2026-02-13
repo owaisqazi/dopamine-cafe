0c7f5cce66e6244ed5c90c27f8cb9c623149e922
owaisqazi <waqqasqaziqazi@gmail.com>
2026-02-13 07:52:50 +0500

    all done

--- components/TawkTo.jsx
+++ components/TawkTo.jsx
@@ -5,14 +5,14 @@ export default function TawkTo() {
   return (
     <Script
       id="tawk-to"
-      strategy="afterInteractive" // client-side load
+      strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
           var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
           (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/667d9864eaf3bd8d4d150ed3/1i1d9mign';
+            s1.src='https://embed.tawk.to/698cf03c0867ad1c3917f78a/1jh78kr70';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
