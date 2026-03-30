a617024aca2228f2db6c6170880aec4f88acebbd
owaisqazi <waqqasqaziqazi@gmail.com>
2026-03-30 19:21:04 +0500

    new script add

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             {children}
             <ContactFloatButton />
             <TawkTo />
+            
           </AOSProvider>
         </ReduxProvider>
       </body>
